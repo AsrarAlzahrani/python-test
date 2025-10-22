f0c4f26dddcaf7b69756c551e98d370ece1cf29f
AsrarAlzahrani <asrar.knani@gmail.com>
2025-10-22 22:40:52 +0300

    add example.py for test

---
+++ example.py
@@ -0,0 +1,8 @@
+def calculate_discount(price, discount):
+    if discount < 0 or discount > 100:
+        return "Invalid discount"
+    final_price = price - (price * discount / 100)
+    return round(final_price, 2)
+
+print(calculate_discount(200, 10))
+print(calculate_discount(150, -5))
