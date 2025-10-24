5e7a859e7ce7088f623c396478db1d274e7fa21b
AsrarAlzahrani <67025625+AsrarAlzahrani@users.noreply.github.com>
2025-10-24 17:14:55 +0300

    Test change for PR Agent
    Removed calculate_discount function and added add function for testing.

--- example.py
+++ example.py
@@ -1,8 +1,3 @@
-def calculate_discount(price, discount):
-    if discount < 0 or discount > 100:
-        return "Invalid discount"
-    final_price = price - (price * discount / 100)
-    return round(final_price, 2)
+def add(a, b):
+    return a + b  # changed for testing
 
-print(calculate_discount(200, 10))
-print(calculate_discount(150, -5))
